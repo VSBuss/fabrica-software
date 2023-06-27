9ef9302e67550c9b4ff7432c6e7d75a5c744c644
00npc <vitor96.buss@hotmail.com>
2023-06-27 11:37:28 -0400

    Att 27/06/2023

--- Menu Lateral/script.js
+++ Menu Lateral/script.js
@@ -2,77 +2,11 @@ const menuLateral = document.querySelector(".menulateral"),
     cadDropdown = document.querySelector(".cadastrar"),
     subMenu = document.querySelector(".submenu");
 
-
     cadDropdown.addEventListener("click", ()=>{
-        if(menuLateral !== "closed" && subMenu == "show"){
-            subMenu.classList.remove("show");
-        } else {
-            menuLateral.classList.remove("closed");
-            subMenu.classList.toggle("show");
-        }
+        menuLateral.classList.toggle("closed");
+        subMenu.classList.toggle("show");
     })
-
 
     menuLateral.addEventListener("click", ()=>{
         menuLateral.classList.toggle("closed");
     })
-
-
-    /*
-    menuLateral.addEventListener("click", ()=>{
-        menuLateral.classList.toggle("closed");
-    })
-    clickfora.addEventListener("click", ()=>{
-        menuLateral.classList.remove("closed");
-        subMenu.classList.remove("show");
-    })
-    */
-
-
-    /*
-    cadDropdown.addEventListener("click", ()=>{
-        if (menuLateral !== "closed"){
-            subMenu.classList.toggle("show");
-            menuLateral.classList.toggle("closed");
-        } else {
-            null
-        }
-    })
-    */
-
-    /*
-    
-    const menuLateral = document.querySelector(".menulateral"),
-    cadDropdown = document.querySelector(".cadastrar"),
-    subMenu = document.querySelector(".submenu"),
-    clickfora = document.querySelector(".menuabertomobile");
-
-    menuLateral.addEventListener("click", ()=>{
-        menuLateral.classList.toggle("closed");
-    })
-
-    clickfora.addEventListener("click", ()=>{
-        menuLateral.classList.remove("closed");
-        subMenu.classList.remove("show");        
-    })
-
-    cadDropdown.addEventListener("click", ()=>{
-        if (menuLateral !== "closed"){
-            subMenu.classList.toggle("show");
-            menuLateral.classList.toggle("closed");
-        } else {
-            subMenu.classList.toggle("show");
-        }
-    })
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    */
