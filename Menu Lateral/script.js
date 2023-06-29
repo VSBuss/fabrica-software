d157cf5f95d0ddc8a392b69f7751eb73929a932b
00npc <vitor96.buss@hotmail.com>
2023-06-29 07:50:48 -0400

    att

--- Menu Lateral/script.js
+++ Menu Lateral/script.js
@@ -1,3 +1,4 @@
+/*
 const menuLateral = document.querySelector(".menulateral"),
     cadDropdown = document.querySelector(".cadastrar"),
     subMenu = document.querySelector(".submenu");
@@ -10,3 +11,38 @@ const menuLateral = document.querySelector(".menulateral"),
     menuLateral.addEventListener("click", ()=>{
         menuLateral.classList.toggle("closed");
     })
+*/
+
+const menuLateral = document.querySelector(".menulateral");
+cadDropdown = document.getElementById("cadastrarButton");
+subMenu = document.querySelector(".submenu");
+
+var subMenuAberto = false
+var clickCadastrar = false
+
+//metodo responsavel por abrir o menu lateral
+menuLateral.addEventListener("click", ()=>{
+   //verifica se o clique foi no botão cadastrar
+    if(clickCadastrar == false){//se não foi ele abre ou fecha o menulateral e o submenu se estiver aberto
+        menuLateral.classList.toggle("closed")
+        fecharSubMenu()
+    }
+    else{// se foi ele apenas n faz nada
+        clickCadastrar = false // seta como false para validar o proximo clique
+    }
+
+
+})
+
+//metodo responsavel por abrir o submenu
+cadDropdown.addEventListener("click", ()=>{
+    //declara que o clique foi no cadastrar
+    clickCadastrar = true
+    subMenu.classList.toggle("show");
+})
+
+//metodo responsavel por fechar o submenu
+function fecharSubMenu(){
+    subMenu.classList.remove("show");
+    subMenuAberto = false
+}
