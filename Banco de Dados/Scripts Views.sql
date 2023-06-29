a822b2983d39ce916b2d98d123c4552a27fa9e2b
00npc <vitor96.buss@hotmail.com>
2023-06-29 11:36:36 -0400

    Att

--- Banco de Dados/Scripts Views.sql
+++ Banco de Dados/Scripts Views.sql
@@ -11,13 +11,14 @@ on produto.id_categoria = categoria.id_categoria
 
 create view pedidos_feitos_por_clientes as
 select * from cliente
-select cliente.nome, pedido_produtos.valor as "Valor total" from cliente
+select cliente.nome as "cliente", pedido.id_pedido as "IDpedido", pedido_produtos.valor as "ValorPedido", pedido_produtos.valor as "ValorProduto", pedido_produtos.quantidade as "qntDoProduto", produto.nome as "NomeProduto"  from cliente
 left join pedido
 	on cliente.id_cliente = pedido.id_cliente
 join pedido_produtos
 	on pedido.id_pedido = pedido_produtos.id_pedido
 join produto
 	on pedido_produtos.id_produto = produto.id_produto
+where pedido_produtos.quantidade > 1
 limit 15
 
 select * from preco_min
--- Menu Lateral/script.js
+++ Menu Lateral/script.js
@@ -1,47 +1,30 @@
-/*
 const menuLateral = document.querySelector(".menulateral"),
-    cadDropdown = document.querySelector(".cadastrar"),
-    subMenu = document.querySelector(".submenu");
-
-    cadDropdown.addEventListener("click", ()=>{
-        menuLateral.classList.toggle("closed");
-        subMenu.classList.toggle("show");
-    })
-
-    menuLateral.addEventListener("click", ()=>{
-        menuLateral.classList.toggle("closed");
-    })
-*/
-
-const menuLateral = document.querySelector(".menulateral");
-cadDropdown = document.getElementById("cadastrarButton");
+cadDropdown = document.getElementById("cadastrarButton"),
 subMenu = document.querySelector(".submenu");
 
 var subMenuAberto = false
 var clickCadastrar = false
 
-//metodo responsavel por abrir o menu lateral
+//método responsável por abrir o menu lateral
 menuLateral.addEventListener("click", ()=>{
    //verifica se o clique foi no botão cadastrar
-    if(clickCadastrar == false){//se não foi ele abre ou fecha o menulateral e o submenu se estiver aberto
+    if(clickCadastrar == false){//se não foi ele abre ou fecha o menu lateral e o sub menu se estiver aberto
         menuLateral.classList.toggle("closed")
         fecharSubMenu()
     }
     else{// se foi ele apenas n faz nada
         clickCadastrar = false // seta como false para validar o proximo clique
     }
-
-
 })
 
-//metodo responsavel por abrir o submenu
+//método responsável por abrir o sub menu
 cadDropdown.addEventListener("click", ()=>{
     //declara que o clique foi no cadastrar
     clickCadastrar = true
     subMenu.classList.toggle("show");
 })
 
-//metodo responsavel por fechar o submenu
+//método responsável por fechar o sub menu
 function fecharSubMenu(){
     subMenu.classList.remove("show");
     subMenuAberto = false
