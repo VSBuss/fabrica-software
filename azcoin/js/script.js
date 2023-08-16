0d993446b8d3ca9ae8fd0c9bf04b7a434d819a87
Vitor Buss <vitor96.buss@hotmail.com>
2023-08-16 11:31:54 -0400

    Menu modificado

--- azcoin/js/script.js
+++ azcoin/js/script.js
@@ -1,9 +1,13 @@
 const menuLateral = document.querySelector(".menulateral"),
 cadDropdown = document.getElementById("submenuButton"),
+cadDropdown2 = document.getElementById("submenuButton2"),
 subMenu = document.querySelector(".submenu"),
-setabaixo = document.querySelector(".setabaixo");
+subMenu2 = document.querySelector(".submenu2"),
+setabaixo = document.querySelector(".setabaixo"),
+setabaixo2 = document.querySelector(".setabaixo2");
 
 var subMenuAberto = false
+var subMenuAberto2 = false
 var clickCadastrar = false
 
 //método responsável por abrir o menu lateral
@@ -12,23 +16,51 @@ menuLateral.addEventListener("click", ()=>{
     if(clickCadastrar == false){//se não foi ele abre ou fecha o menu lateral e o sub menu se estiver aberto
         menuLateral.classList.toggle("closed")
         fecharSubMenu()
+        fecharSubMenu2()
     }
     else{// se foi ele apenas n faz nada
         clickCadastrar = false // seta como false para validar o proximo clique
     }
 })
 
-//método responsável por abrir o sub menu
+//método responsável por abrir o submenu
 cadDropdown.addEventListener("click", ()=>{
     //declara que o clique foi no cadastrar
     clickCadastrar = true
     subMenu.classList.toggle("show");
     setabaixo.classList.toggle("rotate");
 })
+cadDropdown2.addEventListener("click", ()=>{
+    //declara que o clique foi no cadastrar
+    clickCadastrar = true
+    subMenu2.classList.toggle("show");
+    setabaixo2.classList.toggle("rotate");
+})
 
-//método responsável por fechar o sub menu
+//método responsável por fechar o submenu
 function fecharSubMenu(){
     subMenu.classList.remove("show");
     setabaixo.classList.remove("rotate");
     subMenuAberto = false
+}
+function fecharSubMenu2(){
+    subMenu2.classList.remove("show");
+    setabaixo2.classList.remove("rotate");
+    subMenuAberto2 = false
+}
+
+/*GUILHERME*/
+function mostrarSenha(){
+    var inputPass = document.getElementById('senha')
+    var btnShowPass = document.getElementById('btn-senha')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type','text')
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill') 
+    }
+    else{
+        inputPass.setAttribute('type','password')
+        btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill')
+
+    }
 }
