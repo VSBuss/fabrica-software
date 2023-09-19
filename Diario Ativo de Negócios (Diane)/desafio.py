1e534b76d5d44929528d53580b82bd608f0da0c7
Vitor Buss <vitor96.buss@hotmail.com>
2023-09-19 11:38:04 -0400

    Desafioooo

--- Diario Ativo de Negócios (Diane)/desafio.py
+++ Diario Ativo de Negócios (Diane)/desafio.py
@@ -46,6 +46,7 @@ print(df)
 tipo = []
 grupo = []
 
+## Seleciona e insere os nomes dos grupos e tipos de conta nas listas acima.
 x = 0
 sql = "SELECT nome FROM tipo"
 df = pd.read_sql_query(sql, con=engine)
@@ -68,49 +69,117 @@ contas = []
 opcoes = 0
 while opcoes != '3':
     opcoes = input("\n----------DIANE----------\n"
-                       "[1] - Contas\n"
-                       "[2] - Relatórios\n"
-                       "[3] - Sair\n\n")
+                   "[1] - Contas\n"
+                   "[2] - Relatórios\n"
+                   "[3] - Sair\n\n")
     if opcoes == '1':
         opcoes2 = input("\n----------DIANE----------\n\n"
-                            "[1] - Cadastrar Conta\n"
-                            "[2] - Investimento\n"
-                            "[3] - Mostrar Relatório\n"
-                            "[4] - Voltar\n\n")
+                        "[1] - Cadastrar Conta\n"
+                        "[2] - Sugestões de Investimento\n"
+                        "[3] - Voltar\n\n")
         if opcoes2 == '1':
-            receita_despesa = int(input("\nQue tipo de conta deseja cadastrar?\n"
-                                    "[1] - Receita\n"
-                                    "[2] - Despesa\n\n"))
+            receita_despesa = input("\nQue tipo de conta deseja cadastrar?\n"
+                                        "[1] - Despesa\n"
+                                        "[0] - Receita\n\n")
             print("\nComplete os campos abaixo:\n")
             for indice, contador in enumerate(tipo):
                 print(f"[{indice}] - {contador}")
-            descricao = input("Descrição: ")
+            descricao = int(input("Descrição: "))
             grupo = 1
             valor = float(input("Valor: "))
             data_cadastro = date.today()
             data_cadastro = data_cadastro.strftime("%d/%m/%Y")
             data_vencimento = input("Data de vencimento [dd/mm/ano]: ")
-            situacao = input("A conta está paga?\n[1] - SIM\n[2] - NÃO\n")
-            conta = Conta(receita_despesa, descricao, tipo, grupo, valor, data_cadastro, data_vencimento, situacao)
-            conta.cadastrarConta()
-            contas.append(conta)
+            situacao = input("A conta está paga?\n[1] - SIM\n[0] - NÃO\n")
+            if situacao == '1':
+                # Executar uma instrução SQL para inserir dados em uma tabela
+                curs.execute("INSERT INTO conta (receita_despesa, valor, data_cadastro, data_vencimento, data_pg, situacao, id_tipo, id_grupo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                            (receita_despesa, valor, data_cadastro, data_vencimento, data_cadastro, situacao, descricao, grupo))
+                # Confirmar a transação
+                connection.commit()
+                # Fechar a conexão
+                connection.close()
+            elif situacao == '0':
+                # Executar uma instrução SQL para inserir dados em uma tabela
+                curs.execute("INSERT INTO conta (receita_despesa, valor, data_cadastro, data_vencimento, situacao, id_tipo, id_grupo) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                            (receita_despesa, valor, data_cadastro, data_vencimento, situacao, descricao, grupo))
+                # Confirmar a transação
+                connection.commit()
+                # Fechar a conexão
+                connection.close()
+
         elif opcoes2 == '2':
-            a=a
+            1==1
         elif opcoes2 == '3':
-            a=a
-        elif opcoes2 == '4':
-            a=a
+            print("\n"*6)
         else:
             print("\nEntrada Inválida")
         
     elif opcoes == '2':
-        operacoes = input("\n----DIANE----\n"
-                        "----Conta Pessoal----\n"
-                            "1-Boletos\n"
-                            "2-Relatórios\n"
-                            "3-Saldo\n"
-                            "4-Sair\n")
+        opcoes3 = input("\n----------DIANE----------\n"
+                          "------Conta Pessoal------\n"
+                          "[1] - Despesas Pagas\n"
+                          "[2] - Despesas em Aberto\n"
+                          "[3] - Receitas Recebidas\n"
+                          "[4] - Receitas a Receber\n"
+                          "[5] - Relatório Geral\n"
+                          "[6] - Saldo\n"
+                          "[7] - Voltar\n\n")
+        ## DESPESAS PAGAS
+        if opcoes3 == '1': 
+            sql =  "SELECT valor, grupo.nome, tipo.nome FROM conta " \
+                   "JOIN grupo " \
+                   "ON conta.id_grupo = grupo.id_grupo " \
+                   "JOIN tipo " \
+                   "ON conta.id_tipo = tipo.id_tipo " \
+                   "where data_pg is not null"
+            df = pd.read_sql_query(sql, con=engine)
+            pagos = df['valor'].values[0]
+            print(df)
+        
+        ## DESPESAS EM ABERTO
+        elif opcoes3 == '2':     
+            sql = "SELECT * FROM conta WHERE data_pg IS NULL" \
+            "JOIN grupo"
+            df = pd.read_sql_query()
+            print(df)
+            x = 0
+            while x < df.shape[0]:
+                nome = df['nome'].values[x]
+                x = x + 1
+                print(nome)
+
+        ## RECEITAS RECEBIDAS
+        elif opcoes3 == '3': 
+            1==1
+
+        ## RECEITAS A RECEBER
+        elif opcoes3 == '4': 
+            1==1
+
+        ## RELATÓRIO GERAL
+        elif opcoes3 == '5': 
+            1==1
+
+        ## SALDO
+        elif opcoes3 == '6': 
+            sql = "SELECT * FROM saldo"
+            df = pd.read_sql_query(sql, con=engine)
+            saldo = df['valor'].values[0]
+            saldo = float(saldo)
+            print(f"\nSALDO ATUAL: R$ {saldo:.2f}")
+
+        ## VOLTAR
+        elif opcoes3 == '7':
+            print("\n"*6)
+        
+        ## EXCEPTION
+        else:
+            print("\nEntrada Inválida")
+        
     elif opcoes == '3':
         print("\nTchau =)")
     else:
         print("\nEntrada Inválida")
+
+## Tratamento de exceções e calcular e inserir juros
